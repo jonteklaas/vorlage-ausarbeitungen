56fc0022adee59199421bcecbf63a6f7b0f5bb3a
Jonte Klaas <jonte.klaas@gmx.de>
2022-10-19 19:00:35 +0200

    Erstelle Projekt

---
+++ Ausarbeitungen/Vorlage/Listings/Beispiel.php
@@ -0,0 +1,11 @@
+<?php
+class ComparedNaturalModuleInformation
+{
+	const EMPTY_SIGN = 0;
+	const SIGN_OK = 1;
+	const SIGN_NEXT_STEP = 2;
+	const SIGN_CREATE = 3;
+	const SIGN_CREATE_AND_NEXT_STEP = 4;
+	const SIGN_ERROR = 5;
+}
+?>
